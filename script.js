0d892de8154239e524619ebff70c3057328a68b7
ammaryounas339 <ammaryounas339.ay@gmail.com>
2022-01-15 00:40:34 +0500

    does what its supposed to do

--- script.js
+++ script.js
@@ -12,12 +12,145 @@ function toggleEco() {
     document.getElementById("dropdownPh").classList.toggle("show");
   }
   function toggleroom(id){
-  
-    let content = document.getElementById(id).innerHTML = "<a id =id onclick='toggleBack(this.id)'> Staus : Free <br> Rating : 4.2</a>";
-    
-  }
-  function toggleBack(id){
-    
-    
-  }
+    let content = document.getElementById(id);
+   
+     if(id == "room1"){
+      if(content.innerHTML == "Room 1"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 1";
+      }
+     }
+      
+     if(id == "room2"){
+      if(content.innerHTML == "Room 2"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 2";
+      }
+     }
+    if(id == "room3"){
+      if(content.innerHTML == "Room 3"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 3";
+      }
+    }
+    if(id == "room4"){
+      if(content.innerHTML == "Room 4"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 4";
+      }
+    }
+    if(id == "room5"){  
+      if(content.innerHTML == "Room 5"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 5";
+      }
+    }
+    if(id == "room6"){
+      if(content.innerHTML == "Room 6"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 6";
+      }
+    }
+    if(id == "room7"){
+      if(content.innerHTML == "Room 7"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 7";
+      }
+    }
+    if(id == "room8"){
+      if(content.innerHTML == "Room 8"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 8";
+      }
+    }
+    if(id == "room9"){
+      if(content.innerHTML == "Room 9"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 9";
+      }
+    }
+    if(id == "room10"){
+      if(content.innerHTML == "Room 10"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 10";
+      }
+    }
+    if(id == "room11"){
+      if(content.innerHTML == "Room 11"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 11";
+      }
+    }
+    if(id == "room12"){
+      if(content.innerHTML == "Room 12"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 12";
+      }
+    }
+    if(id == "room13"){
+      if(content.innerHTML == "Room 13"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 13";
+      }
+    }
+    if(id == "room14"){
+      if(content.innerHTML == "Room 14"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 14";
+      }
+    }
+    if(id == "room15"){
+      if(content.innerHTML == "Room 15"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 15";
+      }
+    }
+    if(id == "room16"){
+      if(content.innerHTML == "Room 16"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 16";
+      }
+    }
+    if(id == "room17"){
+      if(content.innerHTML == "Room 17"){
+        content.innerHTML= "Status : Free <br> Rating : 4.1";
+      }
+      else {
+        content.innerHTML = "Room 17";
+      }
+    }
 
+  }
+  
