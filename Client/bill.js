eba3c10aa1d9668b4422ec76dd95e308f0e6253e
mishaltariq <81642228+mishaltariq@users.noreply.github.com>
2022-01-19 18:10:52 +0500

    added bill.js

--- Client/bill.js
+++ Client/bill.js
@@ -1,5 +1,28 @@
+window.onload = getBillInfo()
 function displayBill()
 {
     document.getElementById("Bill").style.display="block";
     document.getElementById("checkout").style.display="none";
+}
+function getBillInfo() { 
+    window.alert("hello")
+    $.get("http://localhost:8080/getBillData?id=123",
+    {
+    },
+    function (data, status) {
+        document.getElementById("RoomsBooked").innerHTML = data.rBooked
+        r = data.roomsNo
+            rooms = r[0]
+            for(i = 1;i<r.length;i++){
+                rooms+=r[i]+", "
+            }
+
+        document.getElementById("RoomNo").innerHTML = rooms
+        document.getElementById("Food").innerHTML = data.sFood
+        document.getElementById("Tennis").innerHTML = data.sTennis
+        document.getElementById("Bowling").innerHTML = data.sBowling
+        document.getElementById("Movies").innerHTML = data.sCinema
+
+    });
+
 }
