d9416e387c6e6b0a1f402306b0fe40be5e17ea02
ammaryounas339 <ammaryounas339.ay@gmail.com>
2022-01-25 22:55:03 +0500

    commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const config = {
-    user: 'admin',
-    password: '123',
-    server: 'DESKTOP-TA4RQON', // You can use 'localhost\\instance' to connect to named instance
-    database: 'Resort_DB',
-    port: 1433,
-    trustServerCertificate: true
-}
+// const config = {
+//     user: 'admin',
+//     password: '123',
+//     server: 'DESKTOP-TA4RQON', // You can use 'localhost\\instance' to connect to named instance
+//     database: 'Resort_DB',
+//     port: 1433,
+//     trustServerCertificate: true
+// }
 
 //  const config = {
 //     user: 'mishal',
@@ -16,14 +16,14 @@ const config = {
 //     trustServerCertificate: true
 //  }
 
- // const config = {
-//     user: 'admin1',
-//     password: '123',
-//     server: 'DESKTOP-IO2BR35',
-//     database: 'Resort_DB',
-//     port: 1433,
-//     trustServerCertificate: true
-//   }
+ const config = {
+    user: 'admin1',
+    password: '123',
+    server: 'DESKTOP-IO2BR35',
+    database: 'Resort_DB',
+    port: 1433,
+    trustServerCertificate: true
+  }
 
 
 async function getBillInfo(id) {
